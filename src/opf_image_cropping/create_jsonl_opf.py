b5b679a4d0d2c5d095750b657228d0d473e127af
tenkal <tenkalden007@gmail.com>
2024-07-17 16:36:07 +0530

    updated script to create multiple jsonl file as needed

--- src/opf_image_cropping/create_jsonl_opf.py
+++ src/opf_image_cropping/create_jsonl_opf.py
@@ -2,13 +2,14 @@ import csv
 import json
 import os
 
-def create_jsonl(csv_file, jsonl_ga, jsonl_gb):
+
+def create_jsonl(csv_file, jsonl_files, group_num):
     unique_characters = set()
     jsonl_entries = []
 
     with open(csv_file, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
-        next(reader)  # Skip the header row
+        next(reader)  
         for row in reader:
             char = row[0]
             reference = json.loads(row[4])
@@ -30,23 +31,30 @@ def create_jsonl(csv_file, jsonl_ga, jsonl_gb):
                 }
                 jsonl_entries.append(jsonl_entry)
 
-    split_point = len(jsonl_entries) // 2
+    num_entries = len(jsonl_entries)
+    entries_per_part = num_entries // group_num
 
-    def write_jsonl(entries, file_path):
-        try:
-            with open(file_path, 'w', encoding='utf-8') as f:
-                for entry in entries:
-                    json.dump(entry, f, ensure_ascii=False)
-                    f.write('\n')
-        except IOError as e:
-            print(f"Error writing to file {file_path}: {e}")
+    for part in range(group_num):
+        start_idx = part * entries_per_part
+        end_idx = start_idx + entries_per_part if part < group_num - 1 else num_entries
 
-    write_jsonl(jsonl_entries[:split_point], jsonl_ga)
-    write_jsonl(jsonl_entries[split_point:], jsonl_gb)
+        part_entries = jsonl_entries[start_idx:end_idx]
+        write_jsonl(part_entries, jsonl_files[part])
+
+
+def write_jsonl(entries, file_path):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        for entry in entries:
+            json.dump(entry, f, ensure_ascii=False)
+            f.write('\n')
+
 
 if __name__ == "__main__":
     csv_file_path = '../../data/mapping_csv/derge_char_mapping.csv'
-    jsonl_file_ga_path = '../../data/output_jsonl/derge_opf_ga.jsonl'
-    jsonl_file_gb_path = '../../data/output_jsonl/derge_opf_gb.jsonl'
+    jsonl_file_paths = [
+        '../../data/output_jsonl/derge_opf_ga.jsonl',
+        '../../data/output_jsonl/derge_opf_gb.jsonl'
+    ]
+    group_num = 2
 
-    create_jsonl(csv_file_path, jsonl_file_ga_path, jsonl_file_gb_path)
+    create_jsonl(csv_file_path, jsonl_file_paths, group_num)
