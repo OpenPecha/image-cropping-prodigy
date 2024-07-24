61476ba5fb0c908968db56eed8724ac1edc1d71e
tenkal <tenkalden007@gmail.com>
2024-07-24 08:21:55 +0530

    update to scripts

--- src/opf_image_cropping/create_jsonl_opf.py
+++ src/opf_image_cropping/create_jsonl_opf.py
@@ -25,7 +25,7 @@ def create_jsonl(csv_file, jsonl_files, group_num):
                 unique_characters.add(char)
                 jsonl_entry = {
                     "id": f"{char}.jpg",
-                    "image_url": image_url,
+                    "image": image_url,
                     "text": char,
                     "line_info": line_info
                 }
